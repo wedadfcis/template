2a066044b1f5201702e3a9f2f1e7e046529b561e
GitHub Action <action@github.com>
2021-04-08 12:56:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/wedadfcis/template/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wedadfcis/template/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wedadfcis.template.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wedadfcis.template.MyBundle
 
 class MyApplicationService {
 
